69becbac404e3bab6563052d8a1559bdfe2373bf
TrungNgocCode <trungngoccode@gmail.com>
2022-02-24 17:38:59 +0700

    finish 2c

---
+++ app/src/main/java/com/example/androidtuan2/Activity2c.java
@@ -0,0 +1,14 @@
+package com.example.androidtuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2c);
+    }
+}
